1d8b5a5a3566108d27befab9a3e234c8d7e299bb
Miroslav Krstic <krsticmiroslav99@gmail.com>
2026-05-28 12:44:55 +0200

    feat: add PWA install support

---
+++ public/sw.js
@@ -0,0 +1,83 @@
+const CACHE_NAME = 'anteiku-static-v1';
+const APP_SHELL_URLS = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/anteiku-mark.svg',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/icons/maskable-512.png',
+  '/icons/apple-touch-icon.png',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL_URLS))
+      .then(() => self.skipWaiting()),
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(cacheNames.filter((cacheName) => cacheName !== CACHE_NAME).map((cacheName) => caches.delete(cacheName))),
+      )
+      .then(() => self.clients.claim()),
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  const requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const responseCopy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put('/', responseCopy));
+          return response;
+        })
+        .catch(() => caches.match('/')),
+    );
+    return;
+  }
+
+  const cacheableStaticPath =
+    requestUrl.pathname.startsWith('/assets/') ||
+    requestUrl.pathname.startsWith('/icons/') ||
+    requestUrl.pathname === '/anteiku-mark.svg' ||
+    requestUrl.pathname === '/manifest.webmanifest';
+
+  if (!cacheableStaticPath) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((response) => {
+        if (response.ok) {
+          const responseCopy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, responseCopy));
+        }
+
+        return response;
+      });
+    }),
+  );
+});
